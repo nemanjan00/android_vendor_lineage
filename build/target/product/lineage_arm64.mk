463ba86e81231cc6c8c2b16a336e09ee08c76e43
Nemanja Nedeljković <nemanjan00@gmail.com>
2021-05-25 14:57:39 +0200

    Enable addon sdk

--- build/target/product/lineage_arm64.mk
+++ build/target/product/lineage_arm64.mk
@@ -20,3 +20,6 @@ include vendor/lineage/build/target/product/lineage_generic_target.mk
 TARGET_NO_KERNEL_OVERRIDE := true
 
 PRODUCT_NAME := lineage_arm64
+
+PRODUCT_SDK_ADDON_NAME := lineage
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
